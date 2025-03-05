0b4e0a75b4d126e49847a8b3bcefbef0a1036ac9
Qofy <148817944+Qofy@users.noreply.github.com>
2025-03-05 11:28:05 +0100

    react router navigating programatically(example place order and go back)

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -2,6 +2,7 @@ import {Routes,Route} from "react-router-dom"
 import { Home } from "./component/Home"
 import { About } from "./component/About"
 import { Nav } from "./component/Nav"
+import { OrderSummary } from "./component/OrderSummary"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
      <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="about" element={<About/>}/>
+      <Route path="order-sammary" element={<OrderSummary/>}/>
      </Routes>
     </>
   )
--- react-router/src/component/Home.jsx
+++ react-router/src/component/Home.jsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom"
 export const Home = () => {
+  const navigate = useNavigate()
   return(
     <div>
       <h4>
         Home Page
       </h4>
+      <button onClick={() => navigate("order-sammary")}>Confirm Order</button>
     </div>
   )
 }
