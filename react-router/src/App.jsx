5346ad0736c6c948b26e89d9982bd335e42c9d94
Qofy <148817944+Qofy@users.noreply.github.com>
2025-03-05 10:44:04 +0100

    react-router using links

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,10 +1,12 @@
 import {Routes,Route} from "react-router-dom"
 import { Home } from "./component/Home"
 import { About } from "./component/About"
+import { Nav } from "./component/Nav"
 
 function App() {
   return (
     <>
+    <Nav/>
      <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="about" element={<About/>}/>
